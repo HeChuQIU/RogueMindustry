2b1dba6b5ab3db8ae2c713d893be0cc9c63c65ac
User <user@example.com>
2026-01-03 12:15:06 +0800

    构建环境配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,9 @@
 import io.github.liplum.mindustry.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.21"
-    id("io.github.liplum.mgpp") version "1.3.0"
-}
-
-sourceSets {
-    main {
-        java.srcDirs("src")
-    }
-    test {
-        java.srcDir("test")
-    }
-}
-group= "org.example"
-version= "1.0"
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    id("io.github.liplum.mgpp") version "1.3.2"
+    kotlin("jvm") version "1.9.22"
 }
 repositories {
     mavenCentral()
@@ -29,16 +12,34 @@ repositories {
 dependencies {
     importMindustry()
 }
+sourceSets {
+    main {
+        kotlin.srcDirs("src")
+    }
+    test {
+        kotlin.srcDir("test")
+    }
+}
+group = "io.github.hechuqiu.roguemindustry"
+version = "1.0"
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
 mindustry {
+    projectType = Plugin
     dependency {
-        mindustry on "v146"
-        arc on "v146"
+        mindustry on "v154.2"
+        arc on "v154.2"
     }
     client {
-        mindustry official "v146"
+        mindustry official "v154.2"
     }
     server {
-        mindustry official "v146"
+        mindustry official "v154.2"
     }
     deploy {
         baseName = project.name
---
+++ src/io/github/hechuqiu/roguemindustry/RogueMindustryMod.kt
@@ -0,0 +1,8 @@
+package io.github.hechuqiu.roguemindustry
+
+import mindustry.mod.Mod
+
+class RogueMindustryMod : Mod() {
+    
+}
+
